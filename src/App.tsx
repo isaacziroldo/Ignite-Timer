b2bec8fa56a1111cd13cd6dacf5eedbc8924618d
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-02-21 20:23:25 -0300

    React Router, Route Layout...

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-        <Button />
+      <BrowserRouter>
+        <ThemeProvider theme={defaultTheme}>
+          <Router />
 
-        <GlobalStyle />
-      </ThemeProvider>
+          <GlobalStyle />
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
