17e515c2118a1271eb06497c0b424942117ba452
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-02-27 14:26:23 -0300

    Home Button, Home Inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -15,6 +15,27 @@ export const HomeContainer = styled.main`
   }
 `
 
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
 export const FormContainer = styled.div`
   width: 100%;
   display: flex;
@@ -27,10 +48,18 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `
 
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const CountDownContainer = styled.div`
   font-family: 'Roboto mono', monospace;
-  font-size: 12rem;
-  line-height: 10rem;
+  font-size: 11rem;
+  line-height: 9rem;
   color: ${(props) => props.theme['gray-100']};
 
   display: flex;
@@ -51,4 +80,31 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  font-weight: bold;
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled)hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
