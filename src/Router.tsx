deedd961812eaa8c8fad511f745f9581b0500ef8
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-03-05 16:32:58 -0300

    page history

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout/'
-import { History } from './pages/HIstory'
+import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
